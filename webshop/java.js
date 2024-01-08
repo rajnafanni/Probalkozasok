1e835c851027329633673a54541a1ef8de772caa
rajnafanni <155839917+rajnafanni@users.noreply.github.com>
2024-01-08 14:06:25 +0100

    hiba javítása

--- webshop/java.js
+++ webshop/java.js
@@ -4,7 +4,7 @@ window.addEventListener ("scroll",function(){
     header.classList.toggle ("sticky",this.window.scrollY > 0);
 })
 
-let menu= document.querySelector('#menu.icon');
+let menu= document.querySelector('#menu-icon');
 let navmenu = document.querySelector('.navmenu');
 
 menu.onclick = () => {
